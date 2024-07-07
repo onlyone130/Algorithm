633b184e0badc26c9c617b85cbb27214d5b8adaf
tama <rufina130@naver.com>
2024-07-08 00:33:09 +0900

    5988

---
+++ BEAKJOON/odd_even.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a =  sc.nextInt();
+        if(a != 0){
+            for (int i = 0; i < a; i++) {
+                String[] arr = sc.next().split("");
+                System.out.println(Integer.parseInt(arr[arr.length - 1]) % 2 == 1 ? "odd": "even");
+            }
+        }
+    }
+}
