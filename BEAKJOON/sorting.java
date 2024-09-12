aaaef177598c2fa2e8d565a394bc0a5d3b090f60
tama <rufina130@naver.com>
2024-09-12 13:34:48 +0900

    2750

---
+++ BEAKJOON/sorting.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i=0;i<n-1;i++){
+            for (int j=i+1;j<n;j++){
+                if (arr[i]>arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        for (int val : arr){
+            System.out.println(val);
+        }
+    }
+
+}
