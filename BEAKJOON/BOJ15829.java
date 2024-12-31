398e4516371d67e613dec189a4bba5ba598cea75
tama <rufina130@naver.com>
2024-12-31 14:54:19 +0900

    int long type check

--- BEAKJOON/BOJ15829.java
+++ BEAKJOON/BOJ15829.java
@@ -22,3 +22,33 @@ public class Main {
         System.out.println(sum%m);
     }
 }
+
+/* 100점 만점 코드
+    import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        int r = 31;
+        int m = 1234567891;
+
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        char[] text = sc.next().toCharArray();
+
+        long sum = 0;
+
+        for (int i=0;i<L;i++) {
+            long v = text[i] - 'a' + 1;
+            long a = 1;
+            for (int j=0;j<i;j++) {
+                a = (a*r)%m;
+            }
+            long temp = (v*a)%m;
+            sum += temp;
+            sum %= m;
+        }
+        System.out.println(sum%m);
+    }
+}
+*/
