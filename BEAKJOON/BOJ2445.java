279e64a0d3fbb78c893e182cef9f7247b7176053
tama <rufina130@naver.com>
2025-01-02 14:27:20 +0900

    별찍기8

---
+++ BEAKJOON/BOJ2445.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=0;j<2*n;j++){
+                if(j<=i || i+j>=2*n-1){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for(int i=n; i<2*n; i++){
+            for(int j=0;j<2*n;j++){
+                if(j>i || i+j<2*n-1){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
