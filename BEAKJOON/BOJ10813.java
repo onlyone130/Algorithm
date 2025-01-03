090bf9541f82f61fb0ede1fcf4abcd36e467b4f5
tama <rufina130@naver.com>
2025-01-03 22:54:09 +0900

    공 바꾸기

---
+++ BEAKJOON/BOJ10813.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int a[] = new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            //자신과 번호가 같은 공 넣어주기
+            a[i] = i;
+        }
+        //바꾸기
+        for(int i=0;i<m;i++){
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            //swap 해주기
+            int temp = a[s];
+            a[s] = a[e];
+            a[e] = temp;
+        }
+        for(int i=1;i<=n;i++){
+            System.out.print(a[i]+ " ");
+        }
+    }
+}
