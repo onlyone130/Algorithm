4b752e22b5c70aadd4a87d1c996d1c57325f8c87
tama <rufina130@naver.com>
2025-01-06 18:35:48 +0900

    나는 요리사다

---
+++ BEAKJOON/BOJ2953.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int maxScore = 0;  
+        int winner = 0;    
+
+        for (int i = 0; i < 5; i++) { 
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+
+            for (int j = 0; j < 4; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+
+            if (sum > maxScore) {
+                maxScore = sum;
+                winner = i + 1;
+            }
+        }
+
+        System.out.println(winner + " " + maxScore);
+    }
+}
