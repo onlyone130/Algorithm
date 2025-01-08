c50939934161f701a5a1730c292485ec2c9bdf90
tama <rufina130@naver.com>
2025-01-08 11:19:42 +0900

    열 개씩 끊어 출력하기

---
+++ BEAKJOON/BOJ11721.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i ++) {            
+            System.out.print(s.charAt(i));                        
+            if ((i + 1) % 10 == 0) {                
+                System.out.println();            
+            }        
+        }         
+        br.close();
+    }
+}
