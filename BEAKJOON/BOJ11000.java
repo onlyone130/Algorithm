6f881cc30b6f73df570c82fb26f8865e4dc9b305
tama <rufina130@naver.com>
2025-01-10 09:53:47 +0900

    하얀 칸

---
+++ BEAKJOON/BOJ11000.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        
+        for(int i=0;i<8;i++){
+            String s = br.readLine();
+            for(int j=0;j<8;j++){
+                if(s.charAt(j)=='F' && (i+j)%2==0){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
