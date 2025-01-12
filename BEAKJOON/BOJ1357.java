7aea01d889b0bf8f23d88507e21c069f3148afb8
tama <rufina130@naver.com>
2025-01-12 13:52:44 +0900

    뒤집힌 덧셈

---
+++ BEAKJOON/BOJ1357.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        String x = st.nextToken();
+        String y = st.nextToken();
+ 
+        String RevX = new StringBuilder(x).reverse().toString();
+        String RevY = new StringBuilder(y).reverse().toString();
+
+        int sum = Integer.parseInt(RevX)+Integer.parseInt(RevY);
+
+        String result = new StringBuilder(String.valueOf(sum)).reverse().toString();
+        System.out.println(Integer.parseInt(result));
+    }
+}
