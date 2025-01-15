99c6036c5005661a84ad89004cc248d01c239fa7
tama <rufina130@naver.com>
2025-01-15 16:20:57 +0900

    윷놀이

---
+++ BEAKJOON/BOJ2490.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i < 3; i++) {
+			int sum = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j < 4; j++) {
+				sum += Integer.parseInt(st.nextToken());
+			}
+
+            switch (sum) {
+                case 3:
+                    System.out.println("A");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 1:
+                    System.out.println("C");
+                    break;
+                case 0:
+                    System.out.println("D");
+                    break;
+                default:
+                    System.out.println("E");
+                    break;
+            }
+        }
+    }
+}
