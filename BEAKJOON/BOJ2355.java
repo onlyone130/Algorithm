350c0d74c178a6ce605825b3b58a8317d1c9da6f
tama <rufina130@naver.com>
2025-01-16 11:08:21 +0900

    시그마

---
+++ BEAKJOON/BOJ2355.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        long s = Math.min(a, b);
+        long e = Math.max(a, b);
+
+        long n = e-s+1;
+        long sum = n*(s+e)/2;
+
+        System.out.println(sum);   
+    }
+}
