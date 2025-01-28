85b27c67c26d92ed35b0bb2604ba0b48e5aecf91
tama <rufina130@naver.com>
2025-01-28 15:11:58 +0900

    에디터 (완성 못함)

---
+++ BEAKJOON/C++/BOJ1406.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    
+    int n;
+    std::cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        std::string str;
+        std::cin >> str;
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
