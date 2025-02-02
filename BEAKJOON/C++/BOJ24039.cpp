1b4f815cb3684e3058685f892dc649af46c91b30
tama <rufina130@naver.com>
2025-02-02 11:11:53 +0900

    2021은 무엇이 특별할까?

--- BEAKJOON/C++/BOJ24039.cpp
+++ BEAKJOON/C++/BOJ24039.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool prime_num(int num){
+    if(num==2){
+        return true;
+    }
+    for(int i=2;i<=sqrt(num);i++){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
-    std::cin >> n;
-    
+    cin >> n;
+
+    int last = 0;
+    int sum;
+    int i = 2;
+    while (true){
+        if (prime_num(i)){
+            sum = last*i;
+            if(sum>n){
+                break;
+            }
+            last = i;
+        }
+        i++;
+    }
+    cout << sum << endl;
+    return 0;
 }
