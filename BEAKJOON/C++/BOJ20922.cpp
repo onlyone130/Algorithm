1bb9dd72b557dd7213811bff559dd4ac7d0b5c11
tama <rufina130@naver.com>
2025-02-07 14:41:46 +0900

    겹치는 건 싫어

---
+++ BEAKJOON/C++/BOJ20922.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+//겹치는 건 싫어
+typedef long long ll;
+using namespace std;
+
+const int NMAX = 1000020;
+
+int ary[NMAX];
+int prefix[NMAX];
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+
+    for(int i=1;i<=n;i++){
+        cin >> ary[i];
+    }
+
+    int sum = 0;
+
+    for(int i=0;i<n;i++){
+        
+    }
+}
