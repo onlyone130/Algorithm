8fbef91d940b75e4bd48c329bbc09f807edfcc77
tama <rufina130@naver.com>
2025-02-18 19:33:35 +0900

    피보나치 수2

---
+++ BEAKJOON/C++/BOJ2748.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+//피보나치 수2
+using namespace std;
+typedef long long ll;
+
+vector<ll> memo(91,-1);
+
+ll fib(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    if(memo[n] != -1){
+        return memo[n];
+    }
+    return memo[n] = fib(n-1)+fib(n-2);
+}
+int main(){
+    int n;
+    cin >> n;
+
+    cout << fib(n) << '\n';
+}
