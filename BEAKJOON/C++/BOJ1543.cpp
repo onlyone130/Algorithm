42914ac77988930821a09eb0d78b761b197bbad9
tama <rufina130@naver.com>
2026-01-14 23:12:44 +0900

    문서 검색

---
+++ BEAKJOON/C++/BOJ1543.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+
+// 문서 검색
+using namespace std;
+
+int main(){
+	string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2); 
+
+	int cnt = 0;
+
+	if (s1.size() < s2.size()){
+		cout << 0 << endl;
+		return 0;
+	}
+
+	for (int i = 0; i <= s1.size()-s2.size();){
+		bool check = true;
+		for (int j = 0; j < s2.size(); j++){
+
+			if (s1[i + j] != s2[j]){
+				check = false;
+				break;
+			}
+		}
+		if (check){
+			cnt++;
+			i += s2.size();
+		}
+		else
+			i++;
+	}
+	cout << cnt << '\n';
+}
